9834cbabd2172a6e54512540c0528a0e103763c8
unknown <kwameagyenimboateng64@gmail.com>
2020-03-09 00:13:19 +0000

    added colors

---
+++ constants/Colors.js
@@ -0,0 +1,26 @@
+import { Colors } from "react-native/Libraries/NewAppScreen";
+
+const tintColor = '#2f95dc';
+const transparent = "#dfdfdf";
+const black = "#07050d";
+const grey = "#a8a8a8";
+const ash = "#9fa9ad";
+const yellow = "#fbb61b"
+
+export default {
+  tintColor,
+  transparent,
+  black,
+  grey,
+  ash,
+  yellow,
+  tabIconDefault: ash,
+  tabIconSelected: yellow,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+};
